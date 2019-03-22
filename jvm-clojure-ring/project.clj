f907ca05c1ac3e64344d0439b785b72baa5d8178
Axel Schüssler <axel.schuessler@novatec-gmbh.de>
2019-03-22 08:16:26 +0100

    Add JSON support for Clojure example

--- jvm-clojure-ring/project.clj
+++ jvm-clojure-ring/project.clj
@@ -4,7 +4,8 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring-json-response "0.2.0"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler jvm-clojure-ring.handler/app}
   :profiles
--- jvm-clojure-ring/test/jvm_clojure_ring/handler_test.clj
+++ jvm-clojure-ring/test/jvm_clojure_ring/handler_test.clj
@@ -1,19 +1,21 @@
 (ns jvm-clojure-ring.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
+            [cheshire.core :refer [parse-string]]
             [jvm-clojure-ring.handler :refer :all]))
 
 (deftest hello-world-challenge
-  (testing "greeting Adam"
-    (let [response (app (mock/request :post "/say-hello?name=Adam"))]
-      (is (= (:body response) "Hello Adam!"))))
-  (testing "greeting Eve"
-    (let [response (app (mock/request :post "/say-hello?name=Eve"))]
-      (is (= (:body response) "Hello Eve!"))))
   (testing "default response"
     (let [response (app (mock/request :get "/say-hello"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World!"))))
+      (is (= "{\"message\":\"Hello World!\"}" (:body response)))))
+  (letfn [(message [r] (:message (parse-string (:body r) true)))]
+    (testing "greeting Adam"
+      (let [response (app (mock/request :post "/say-hello?name=Adam"))]
+        (is (= "Hello Adam!" (message response)))))
+    (testing "greeting Eve"
+      (let [response (app (mock/request :post "/say-hello?name=Eve"))]
+        (is (= "Hello Eve!" (message response))))))
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
